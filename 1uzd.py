6af748a4f228c38b1170c2568e990cd334528707
GustavsGaldiks <gustavs.galdiks@grobinasskola.lv>
2023-11-15 18:32:38 +0200

    lol

--- 1uzd.py
+++ 1uzd.py
@@ -3,3 +3,6 @@ izveidot uzdevumu kura prasa lietotajaam ievadit
  divus skaitlus un aprekina to summu izvadot 
  terminali
  """
+a=int(input('ievasi 1. sk.'))
+b=int(input('ievadi otro skaitli'))
+c=a+b
